7bb379b1b2016fbe2f13bb219f731feae4e39c84
Keiji Miyazaki <miyah.dev@gmail.com>
2021-07-25 14:43:06 +0900

    Remove unused code

---
+++ server/src/constant.ts
@@ -0,0 +1,1 @@
+export const SOURCE_NAME = 'vue-css-modules-extension'
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,26 +1,24 @@
-import * as path from 'path'
 import {
   createConnection,
   TextDocuments,
-  Diagnostic,
-  DiagnosticSeverity,
   ProposedFeatures,
   InitializeParams,
   DidChangeConfigurationNotification,
   CompletionItem,
-  CompletionItemKind,
   TextDocumentPositionParams,
   TextDocumentSyncKind,
-  InitializeResult
+  InitializeResult,
 } from 'vscode-languageserver/node';
 import {
   getCSSLanguageService,
   getSCSSLanguageService,
   getLESSLanguageService,
-  TextDocument
+  CompletionItemKind,
+  TextDocument,
+  SymbolInformation
 } from 'vscode-css-languageservice';
-import { SymbolKind } from 'vscode-languageserver-types'
-import { VueParser } from './parser'
+import { SymbolKind, } from 'vscode-languageserver-types';
+import { VueParser } from './parser';
 
 // Create a connection for the server, using Node's IPC as a transport.
 // Also include all preview / proposed LSP features.
@@ -42,26 +40,20 @@ const getLanguageService = (fileExtension: string) => {
     default:
       return getCSSLanguageService;
   }
-}
+};
 
-let completionItems: CompletionItem[] = []
+let completionItems: CompletionItem[] = [];
+const compItemMap = new Map<SymbolInformation['name'], CompletionItem>();
+
 
 connection.onInitialize((params: InitializeParams) => {
   const capabilities = params.capabilities;
 
   // Does the client support the `workspace/configuration` request?
   // If not, we fall back using global settings.
-  hasConfigurationCapability = !!(
-    capabilities.workspace && !!capabilities.workspace.configuration
-  );
-  hasWorkspaceFolderCapability = !!(
-    capabilities.workspace && !!capabilities.workspace.workspaceFolders
-  );
-  hasDiagnosticRelatedInformationCapability = !!(
-    capabilities.textDocument &&
-    capabilities.textDocument.publishDiagnostics &&
-    capabilities.textDocument.publishDiagnostics.relatedInformation
-  );
+  hasConfigurationCapability = !!capabilities.workspace?.configuration;
+  hasWorkspaceFolderCapability = !!capabilities.workspace?.workspaceFolders;
+  hasDiagnosticRelatedInformationCapability = !!capabilities.textDocument?.publishDiagnostics?.relatedInformation;
 
   const result: InitializeResult = {
     capabilities: {
@@ -79,6 +71,8 @@ connection.onInitialize((params: InitializeParams) => {
       }
     };
   }
+
+  connection.console.log('#### onInitialize ####');
   return result;
 });
 
@@ -92,28 +86,25 @@ connection.onInitialized(() => {
       connection.console.log('Workspace folder change event received.');
     });
   }
+  connection.console.log('#### onInitialized ####');
 });
 
-// The example settings
-interface ExampleSettings {
+interface Settings {
   maxNumberOfProblems: number;
 }
 
-// The global settings, used when the `workspace/configuration` request is not supported by the client.
-// Please note that this is not the case when using this server with the client provided in this example
-// but could happen with other clients.
-const defaultSettings: ExampleSettings = { maxNumberOfProblems: 1000 };
-let globalSettings: ExampleSettings = defaultSettings;
+const defaultSettings: Settings = { maxNumberOfProblems: 1000 };
+let globalSettings: Settings = defaultSettings;
 
 // Cache the settings of all open documents
-const documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
+const documentSettings: Map<string, Thenable<Settings>> = new Map();
 
 connection.onDidChangeConfiguration(change => {
   if (hasConfigurationCapability) {
     // Reset all cached document settings
     documentSettings.clear();
   } else {
-    globalSettings = <ExampleSettings>(
+    globalSettings = <Settings>(
       (change.settings.languageServerExample || defaultSettings)
     );
   }
@@ -122,7 +113,7 @@ connection.onDidChangeConfiguration(change => {
   documents.all().forEach(validateTextDocument);
 });
 
-function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
+function getDocumentSettings(resource: string): Thenable<Settings> {
   if (!hasConfigurationCapability) {
     return Promise.resolve(globalSettings);
   }
@@ -140,106 +131,68 @@ function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
 // Only keep settings for open documents
 documents.onDidClose(e => {
   documentSettings.delete(e.document.uri);
+  connection.console.log('#### onDidClose ####');
 });
 
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
 documents.onDidChangeContent(change => {
   validateTextDocument(change.document);
+  connection.console.log('#### onDidChangeContent ####');
 });
 
 async function validateTextDocument(textDocument: TextDocument): Promise<void> {
-  // In this simple example we get the settings for every validate run.
-  const settings = await getDocumentSettings(textDocument.uri);
-
-  // The validator creates diagnostics for all uppercase words length 2 and more
-  const text = textDocument.getText();
-
   // Reset complitionItem array
-  completionItems.length = 0
+  compItemMap.clear();
+  completionItems.length = 0;
 
-  const filename = textDocument.uri.split('/').pop() || ''
-  const parser = new VueParser({ source: textDocument.getText(), filename })
-  const styles = parser.getStyles() || []
+  const filename = textDocument.uri.split('/').pop() || '';
+  const parser = new VueParser({ source: textDocument.getText(), filename });
+  const styles = parser.getStyles() || [];
 
   // array of style tags `<style></style>`
   for (let i = 0; i < styles.length; i++) {
-    const style = styles[i]
-    const isModule = style?.module
+    const style = styles[i];
+    const isModule = !!style.module;
     if (!isModule) {
-      continue
+      continue;
     }
 
-    const lang = style?.lang || 'css'
-    const languageService = getLanguageService(lang)
-    const service = languageService()
-    const document = TextDocument.create(textDocument.uri, lang, 0, style.content)
-    const styleSheet = service.parseStylesheet(document)
-    const documentSymbols = service.findDocumentSymbols(document, styleSheet)
+    const lang = style.lang || 'css';
+    const languageService = getLanguageService(lang);
+    const service = languageService();
+    const document = TextDocument.create(textDocument.uri, lang, 0, style.content);
+    const styleSheet = service.parseStylesheet(document);
+    const documentSymbols = service.findDocumentSymbols(document, styleSheet);
 
     // array of class name of Id (ex. `.hello` or `#App` )
     for (let k = 0; k < documentSymbols.length; k++) {
       const sym = documentSymbols[k];
-      const isClassSymbol = sym.kind === SymbolKind.Class
-      const className = sym.name.slice(0, 1) === '.' ? sym.name.slice(1) : null // expected "." or "#"
-      if (!(isClassSymbol && className)) {
-        continue
+      const isClassSymbol = sym.kind === SymbolKind.Class;
+      const className = sym.name.slice(0, 1) === '.' ? sym.name.slice(1) : null; // expected "." or "#"
+
+      if (!isClassSymbol || !className) {
+        continue;
       }
+
       const completionItem: CompletionItem = {
         label: className,
-        kind: CompletionItemKind.Field,
+        kind: CompletionItemKind.Property,
+      };
+
+      if (!compItemMap.get(className)) {
+        compItemMap.set(className, completionItem);
       }
-      completionItems.push(completionItem)
     }
 
-    connection.console.log(JSON.stringify(completionItems, null, '\t'))
-    connection.console.log(JSON.stringify(completionItems.length, null, '\t'))
+    completionItems = [...compItemMap.values()];
+    connection.console.log(JSON.stringify(completionItems, null, '\t'));
   }
-
-  const pattern = /\b[A-Z]{2,}\b/g;
-  let m: RegExpExecArray | null;
-
-  let problems = 0;
-  const diagnostics: Diagnostic[] = [];
-  while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
-    problems++;
-    const diagnostic: Diagnostic = {
-      severity: DiagnosticSeverity.Warning,
-      range: {
-        start: textDocument.positionAt(m.index),
-        end: textDocument.positionAt(m.index + m[0].length)
-      },
-      message: `${m[0]} is all uppercase.`,
-      source: 'ex'
-    };
-    if (hasDiagnosticRelatedInformationCapability) {
-      diagnostic.relatedInformation = [
-        {
-          location: {
-            uri: textDocument.uri,
-            range: Object.assign({}, diagnostic.range)
-          },
-          message: 'Spelling matters'
-        },
-        {
-          location: {
-            uri: textDocument.uri,
-            range: Object.assign({}, diagnostic.range)
-          },
-          message: 'Particularly for names'
-        }
-      ];
-    }
-    diagnostics.push(diagnostic);
-  }
-
-  // Send the computed diagnostics to VSCode.
-  connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
 }
 
 connection.onDidChangeWatchedFiles(_change => {
   // Monitored files have change in VSCode
-  connection.console.log('We received an file change event');
+  connection.console.log('#### onDidChangeWatchedFiles ####');
 });
 
 // This handler provides the initial list of the completion items.
@@ -248,6 +201,7 @@ connection.onCompletion(
     // The pass parameter contains the position of the text document in
     // which code complete got requested. For the example we ignore this
     // info and always provide the same completion items.
+    connection.console.log('#### onCompletion ####');
     return completionItems;
   }
 );
@@ -263,6 +217,7 @@ connection.onCompletionResolve(
       item.detail = 'World CSS';
       item.documentation = 'World CSS documentation';
     }
+    connection.console.log('#### onCompletionResolve ####');
     return item;
   }
 );
